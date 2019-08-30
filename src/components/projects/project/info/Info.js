6b04999d71d01b1effc2d94fd67ad932086e3990
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-08-29 22:23:05 -0400

    fix: class

--- src/components/projects/project/info/Info.js
+++ src/components/projects/project/info/Info.js
@@ -5,7 +5,7 @@ const Info = (props) => {
         <section className="project-info">
             <h3 className="project-name"><a href={props.livePage}>{props.name}</a></h3>
             <p className="project-desription">{props.description}</p>
-            <a href={props.repo}><i class="fab fa-github fa-2x"></i>See How</a>
+            <a href={props.repo}><i className="fab fa-github fa-2x"></i></a>
         </section>
     )
 }
