64e15d6e32e28e6123f194ba182518506fdfb982
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-09-15 16:54:49 -0400

    chore: cleanup header spacing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import ContactForm from './components/contact-form/ContactForm';
 import Header from './components/header/Header';
+import Projects from './components/projects/Projects';
 
 class App extends Component {
     render() {
         return (
-            <>
+            <div className="container">
                 <Header />
+                <Projects />
                 <ContactForm />
-            </>
+            </div>
         );
-
     }
 }
 
