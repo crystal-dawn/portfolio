9f2f1ae3c4067880687579d6c2f9a0a821e133d4
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-09-15 22:16:31 -0400

    feat: honeycomb clusters

--- src/components/projects/project/Project.js
+++ src/components/projects/project/Project.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Project.css';
 import Skills from './skills/Skills';
 import Info from './info/Info';
+import Honeycomb from '../../honeycomb/Honeycomb';
 
 const Project = (props) => (
     <div className="project">
         <Info {...props} />
         <div className="skills">
-            {props.skills.map((skill, index) => <Skills key={index} skill={skill} />)}
+            {props.skills.map((skill, index) => (
+                <div className="skill-honeycomb">
+                    <Honeycomb className="citrus skill" />
+                    <Honeycomb className="citrus skill" />
+                    <Skills key={index} skill={skill} />
+                    <Honeycomb className="citrus skill" />
+                    </div>
+                    )
+            )}
         </div>
     </div>
 )
--- src/components/projects/project/info/Info.js
+++ src/components/projects/project/info/Info.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Info = (props) => {
     return (
         <section className="project-info">
-            <h3 className="project-name"><a href={props.livePage}>{props.name}</a></h3>
-            <p className="project-desription">{props.description}</p>
-            <a href={props.repo}><i className="fab fa-github fa-2x"></i></a>
+            <h3 className="project-links">
+                <a href={props.repo}><i className="fab fa-github fa-2x"></i></a>
+                <a href={props.livePage}>{props.name}</a>
+            </h3>
+            <p className="project-description">{props.description}</p>
         </section>
     )
 }
