31efb61b81545e002e3f29ede6716bf15cf0eaba
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-09-28 15:28:41 -0400

    chore: cleanup unused files

--- README.md
+++
@@ -1,6 +0,0 @@
-#React send emails with Gmail
-
-1) Clone
-2) `npm run dev`
-
-.env needs created with client id, client secret, refresh token
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Projects.css';
 import projects from "./Projects.json";
 import Project from './project/Project';
+import axios from 'axios'
 
 export default class Projects extends Component {
     constructor(props) {
@@ -9,6 +10,28 @@ export default class Projects extends Component {
         this.state = {
             projects
         }
+    }
+
+    componentDidMount() {
+        axios
+            .get(
+                window.encodeURI(
+                    `https://api.github.com/users/crystal-dawn?client_id=eba91468caaaab193ec0&client_secret=97f58e109ec1200b2e1bf536b3422b416ba795de`,
+                ),
+            )
+            .then(response => {
+                const repos = response.data;
+                this.setState({
+                    repos,
+                    loading: false,
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    error: error,
+                    loading: false,
+                });
+            });
     }
 
     render() {
