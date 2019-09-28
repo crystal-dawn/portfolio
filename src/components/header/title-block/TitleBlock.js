aebba39c7038d5f380c4ea35c3f31cd3a80fb4a1
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-09-28 14:17:05 -0400

    chore: cleanup title block styling

--- src/components/header/title-block/TitleBlock.js
+++ src/components/header/title-block/TitleBlock.js
@@ -6,9 +6,9 @@ import Honeycomb from '../../honeycomb/Honeycomb';
 const TitleBlock = () => {
     return (
         <section className="title-block">
+            <Honeycomb className="citrus" />
             <h1 className="name">Crystal Yungwirth</h1>
             <h2 className="title">Front-End <br />Web Developer</h2>
-            <Honeycomb className="citrus" />
         </section>
     )
 }
